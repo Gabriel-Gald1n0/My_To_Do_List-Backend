caf82c6641e98f732b1bd5a0a3f51a678b00384e
gabriel <galdinobrossx1@gmail.com>
2024-04-26 01:03:40 -0300

    novas alterações

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: false }));
 
   app.enableCors({
-    origin: 'http://localhost:3001', // quais endereços de ip podem acessar a API
+    origin: 'http://localhost:3000', // quais endereços de ip podem acessar a API
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Metodos utilizados
     allowedHeaders: 'Content-Type, Accept', //quais headers podem ser enviados
   });
   //app.enableCors(); //permite que qualquer endereço de ip acesse a API
-  const port = app.get(ConfigService).get('PORT') || 3000;
+  const port = app.get(ConfigService).get('PORT') || 3001;
   await app.listen(port);
 }
 bootstrap();
